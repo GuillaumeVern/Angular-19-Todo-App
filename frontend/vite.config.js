017f0eb4098f4782a39daf313737b2c4dfeb6932
Guillaume Vern <guillaumev@zenbook.losvernos.local>
2025-06-30 15:51:06 +0200

    fix: remove vite config

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: ['podman.losvernos.com'], // or ['.losvernos.com'] for a wildcard
-  },
-});
