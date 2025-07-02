a2033974e83dad624a575ab2547f2e9d24f99e22
Guillaume Vern <guillaumev@zenbook.losvernos.local>
2025-07-02 12:53:02 +0200

    feat: remove property from backend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,13 +5,21 @@ import { WeatherComponent } from './components/weather/weather.component';
 import { HangingGameComponent } from './hanging-game/hanging-game.component';
 
 export const routes: Routes = [{
- path:'tasks', component:DragDropComponent,pathMatch:'full'
-
-},{
-  path:'todos',component:TodoComponent,pathMatch:'full'
-}, { path: '', redirectTo: 'todos', pathMatch: 'full' },{
-  path:'weather',component:WeatherComponent,pathMatch:'full'
-},{ path: 'guess-word', component:HangingGameComponent,pathMatch: 'full' },{
-  path:'weather',component:WeatherComponent,pathMatch:'full'
+  path: 'tasks', component: DragDropComponent, pathMatch: 'full'
+},
+{
+  path: 'todos', component: TodoComponent, pathMatch: 'full'
+},
+{
+  path: '', redirectTo: 'todos', pathMatch: 'full'
+},
+{
+  path: 'weather', component: WeatherComponent, pathMatch: 'full'
+},
+{
+  path: 'guess-word', component: HangingGameComponent, pathMatch: 'full'
+},
+{
+  path: 'weather', component: WeatherComponent, pathMatch: 'full'
 }
 ];
